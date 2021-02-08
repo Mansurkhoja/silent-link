2274c666006a75d980db9c24f2d52decaea53425
ManSur Temirkhojaev <you@example.com>
2021-02-08 16:38:14 +0500

    scale for touch

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-"use strict"; // for svg support
-
-svg4everybody(); //for loader
-
+"use strict";
+// for svg support
+svg4everybody();
+//for loader
 var loader = document.getElementById('form-loader');
 
 function loaderShow() {
-  loader.classList.add('loading');
+    loader.classList.add('loading');
 }
 
 function loaderHide() {
-  loader.classList.remove('loading');
+    loader.classList.remove('loading');
+}
+//for touch scale plan
+var planItem = document.getElementsByClassName('plan__item');
+for (let i = 0; i < planItem.length; i++) {
+    planItem[i].addEventListener('touchstart', function() {
+        planItem[i].classList.add('scale');
+    })
+    planItem[i].addEventListener('touchend', function() {
+        planItem[i].classList.remove('scale');
+    })
 }
