9540e0bbc8fd74905f5a8448b1dd94b197c1a97e
ManSur Temirkhojaev <you@example.com>
2021-02-08 14:19:51 +0500

    bugs fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-"use strict";
- // for svg support
-svg4everybody();
+"use strict"; // for svg support
+
+svg4everybody(); //for loader
+
+var loader = document.getElementById('form-loader');
+
+function loaderShow() {
+  loader.classList.add('loading');
+}
+
+function loaderHide() {
+  loader.classList.remove('loading');
+}
